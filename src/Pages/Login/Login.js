a33115592a7177f6c5d72c4a8ab3c1c1cb0fa412
Md Galib Hossain <mdgalib23@gmail.com>
2022-11-26 14:52:06 +0600

    Firebase setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,41 +4,62 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  // react hook form
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const handleLogin = (data) => {
+    console.log(data);
+  };
   return (
     <div className="h-[600px] flex justify-center items-center">
       <div className="w-96 p-7">
         <h2 className="text-2xl">Login</h2>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          {/* email */}
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
               type="text"
-              {...register("email")}
-              className="input input-bordered w-full max-w-xs input-accent"
+              {...register("email", { required: "Email is required" })}
+              className="input input-bordered w-full max-w-xs input-error"
             />
+            {errors.email && (
+              <p className="text-left text-red-500" role="alert">
+                {errors.email?.message}
+              </p>
+            )}
           </div>
+          {/* password */}
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
             <input
               type="password"
-              {...register("password")}
-              className="input input-bordered w-full max-w-xs input-accent"
+              {...register("password", { required: "Password is required" })}
+              className="input input-bordered w-full max-w-xs input-error"
             />
+            {errors.password && (
+              <p className="text-left text-red-500" role="alert">
+                {errors.password?.message}
+              </p>
+            )}
             <label className="label">
-              <span className="label-text">Forget Password!</span>
+              <Link to="/signup">
+                <span className="label-text">Don't have an account?</span>
+              </Link>
             </label>
           </div>
 
+          <input className="btn btn-error w-full" value="Login" type="submit" />
           <input
-            className="btn btn-accent w-full"
-            value="Login"
-            type="submit"
+            className="btn mt-3 btn-outline  w-full"
+            value="Login with Google"
           />
         </form>
       </div>
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,45 +1,114 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../context/AuthProvider";
 
 const Signup = () => {
+  const { createUser } = useContext(AuthContext);
+
+  // react hook form
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const handleSignup = (data) => {
+    createUser(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div className="hero min-h-screen ">
-      <div className="hero-content flex-col">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl my-5 font-bold">Please Signup now!</h1>
-        </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <Link to="/login" className="label-text-alt link link-hover">
-                  Already have an account?
-                </Link>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">SignUp</button>
-            </div>
+    <div className="h-[600px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-2xl">Signup</h2>
+        <form onSubmit={handleSubmit(handleSignup)}>
+          {/* select */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Choose your account type</span>
+            </label>
+            <select
+              {...register("usertype", { required: true })}
+              className="select select-bordered select-error"
+            >
+              <option selected value="Buyer">
+                Buyer
+              </option>
+              <option value="Seller">Seller</option>
+            </select>
           </div>
-        </div>
+          {/* Name */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              {...register("name")}
+              className="input input-bordered w-full max-w-xs input-error"
+            />
+            {errors.name && (
+              <p className="text-left text-red-500" role="alert">
+                {errors.name?.message}
+              </p>
+            )}
+          </div>
+
+          {/* email */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              {...register("email", { required: "Email is required" })}
+              className="input input-bordered w-full max-w-xs input-error"
+            />
+            {errors.email && (
+              <p className="text-left text-red-500" role="alert">
+                {errors.email?.message}
+              </p>
+            )}
+          </div>
+          {/* password */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "password must be at least 6 characters",
+                },
+              })}
+              className="input input-bordered w-full max-w-xs input-error"
+            />
+            {errors.password && (
+              <p className="text-left text-red-500" role="alert">
+                {errors.password?.message}
+              </p>
+            )}
+            <label className="label">
+              <Link to="/login">
+                <span className="label-text">Already have an account?</span>
+              </Link>
+            </label>
+          </div>
+
+          <input
+            className="btn btn-error w-full"
+            value="Signup"
+            type="submit"
+          />
+        </form>
       </div>
     </div>
   );
